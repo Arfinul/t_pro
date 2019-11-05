72449beb6028032dfbdbb0a0d516c3b1070f64e5
Arfinul <mdarfinulhaque@gmail.com>
2019-11-05 17:53:51 +0530

    updated gui

--- gui.py
+++ gui.py
@@ -10,13 +10,15 @@ import datetime
 import time
 import sys
 import subprocess
+from time import sleep
+import requests
+import json
 
 is_login = False
 userName = ""
-cmd = 'time ./uselib cfg/1_black_conveyor.names cfg/5_yolov3_optimised.cfg 5_yolov3_optimised.weights web_camera'
+cmd = 'time ./uselib cfg/1_black_conveyor.names cfg/5_yolov3_optimised.cfg 5_yolov3_optimised.weights web_camera > output.txt'
 
 def vp_start_gui():
-    #####Window is our Main frame of system
     global window
     window = tk.Tk()
     # window.attributes("-fullscreen", True)
@@ -25,47 +27,87 @@ def vp_start_gui():
     window.geometry('1920x1080')
     window.configure(background='snow')
 
-    # # Create a label in the frame
-    # lmain = Label(window)
-    # lmain.place(x=400, y=160)
-
-    # cap = cv2.VideoCapture(0)
-
     # function for video streaming
     def video_stream():
         try:
             global p
             p = subprocess.Popen("exec " + cmd, stdout= subprocess.PIPE, shell=True)
-            # _, frame = cap.read()
-            # frame = cv2.resize(frame, (800,500))
-            # cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-            # img = Image.fromarray(cv2image)
-            # imgtk = ImageTk.PhotoImage(image=img)
-            # lmain.imgtk = imgtk
-            # lmain.configure(image=imgtk)
-            # lmain.after(1, video_stream)
-            endRecord.configure(state="active")
-            startRecord.configure(state="disabled")
+            endRecord.configure(state="active", bg="#539051")
+            startRecord.configure(state="disabled", bg='silver')
         except:
             p.kill()
-            # cap.release()
-            # cv2.destroyAllWindows()
-            endRecord.configure(state="disabled")
-            startRecord.configure(state="active")
+            endRecord.configure(state="disabled", bg='silver')
+            startRecord.configure(state="active", bg="#539051")
             refresh()
 
     def end_video():
         p.kill()
-        # cap.release()
-        # cv2.destroyAllWindows()
-        endRecord.configure(state="disabled")
-        startRecord.configure(state="active")
+        endRecord.configure(state="disabled", bg='silver')
+        startRecord.configure(state="active", bg="#539051")
+        # send_data_api()       //send data to api
         refresh()
 
     def on_closing():
         from tkinter import messagebox
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             window.destroy()
+
+    # APIs
+    def login_api(usr, pwd):
+        url = ""
+        data = {
+            "username": usr,
+            "password": pwd,
+            "entity": "mobile",
+            "deviceToken": "1"
+        }
+        response = requests.request("POST", url, params=json.dumps(data))
+        print(response.json())
+        status = False
+        try:
+            global userID
+            global token
+            userID = response.json()["id"]
+            token = response.json()['token']
+            status = True
+        except Exception as e:
+            print("Exception during login: ", e)
+        return status
+
+
+    def send_data_api():
+        txt_file = open("example.txt", "r").read()
+        li = txt_file.split("\n")
+        _1lb = li[1].split(" : ")[1]
+        _2lb = li[2].split(" : ")[1]
+        _3lb = li[3].split(" : ")[1]
+        url = ""
+        headers = {
+        	"application": ""
+        }
+        data = {
+            "flcData": "",
+            "sectionId": 1,
+            "oneLeafBud": _1lb,
+            "twoLeafBud": _2lb,
+            "oneLeafBanjhi": "",
+            "twoLeafBanjhi": "",
+            "moderateShoot": "",
+            "fineShoot": "",
+            "scannedBy": userID,
+            "weighment": "",
+            "oneBanjhiCount": "",
+            "oneBudCount": "",
+            "oneLeafCount": "",
+            "twoLeafCount": "",
+            "threeLeafCount": "",
+            "userId": 1,
+            "dateDone": datetime.datetime.today().date()
+        }
+        response = requests.request("POST", url, params=json.dumps(data), headers=headers)
+        print(response.json())
+
+
 
     # Designing window for registration
      
@@ -99,10 +141,12 @@ def vp_start_gui():
     # Designing window for login 
      
     def login():
+        x = window.winfo_x()
+        y = window.winfo_y()
         global login_screen
         login_screen = Toplevel(window)
         login_screen.title("Login")
-        login_screen.geometry("300x250")
+        login_screen.geometry("+%d+%d" % (x + 830, y + 230))
         Label(login_screen, text="Please enter details below to login", font=("times", 14, 'bold'), width=30).pack()
         Label(login_screen, text="").pack()
      
@@ -140,13 +184,16 @@ def vp_start_gui():
         username_entry.delete(0, END)
         password_entry.delete(0, END)
      
-        Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+        Label(register_screen, text="Registration Success", fg="#539051", font=("calibri", 11)).pack()
      
     # Implementing event on login button 
      
     def login_verify():
         username1 = username_verify.get()
         password1 = password_verify.get()
+
+        # status = login_api(username1, password1)
+
         username_login_entry.delete(0, END)
         password_login_entry.delete(0, END)
      
@@ -167,20 +214,17 @@ def vp_start_gui():
     # Designing popup for login success
      
     def login_sucess():
-        # global login_success_screen
-        # login_success_screen = Toplevel(login_screen)
-        # login_success_screen.title("Success")
-        # login_success_screen.geometry("150x100")
-        # Label(login_success_screen, text="Login Success").pack()
-        # Button(login_success_screen, text="OK", command=delete_login_success).pack()
         global is_login
         is_login = True
         login_screen.destroy()
         startRecord.place(x=90, y=450)
+        endRecord.configure(state="disabled", bg="silver", fg="black")
         endRecord.place(x=90, y=550)
-        register.place_forget()
+        # register.place_forget()
         signin.place_forget()
-        section.place(x=90, y=150)
+        section_gui()
+        panel_bg.place_forget()
+        # section.place(x=90, y=150)
         global userName
         txt = "Welcome, " + userName.title()
         Label(window, text=txt, font=('times', 15, 'bold'), bg='white').place(x=1550, y=130)
@@ -219,78 +263,78 @@ def vp_start_gui():
         user_not_found_screen.destroy()
 
     def section_gui():
-        global section_screen
-        section_screen = Toplevel(window)
-        section_screen.title("Register")
-        section_screen.geometry("400x300")
-     
+        # section.configure(state="disabled")
         global section_name
         global section_entry
+        global old_button
         section_name = StringVar()
-        Label(section_screen, text="").pack()
-        Label(section_screen, text="Please enter section name", font=("times", 14, 'bold'), width=30).pack()
-        Label(section_screen, text="").pack()
-        section_lable = Label(section_screen, text="Section name * ", font=("times", 13))
-        section_lable.pack()
-        username_entry = Entry(section_screen, textvariable=section_name)
-        username_entry.pack()
-        Label(section_screen, text="").pack()
-        Button(section_screen, text="Add Section", width=10, height=1, bg="salmon", command = add_section, font=("times", 13, 'bold')).pack()
-
+        # name_label = Label(window, text="Please enter section name", font=("times", 14, 'bold'), width=30).place(x=100, y=150)
+        section_lable = Label(window, text="Section name * ", font=("times", 13, "bold"))
+        section_lable.place(x=180, y=160)
+        section_entry = Entry(window, textvariable=section_name)
+        section_entry.place(x=140, y=215)
+        tk.Button(window, text="Add Section", width=10, height=1, bg="#539051", fg="white", command = add_section, font=("times", 13, 'bold')).place(x=110, y=240)
+        tk.Button(window, text="Add Another", width=10, height=1, bg="silver", fg="black", command = refresh, font=("times", 13, 'bold')).place(x=250, y=240)
+       
     def add_section():
+        global section_success
         section_info = section_name.get()
-     
-        file = open(userName, "a")
-        file.write(str(datetime.datetime.now()) + ", ")
-        file.write(section_info + "\n")
-        file.close()
-     
-        Label(section_screen, text="Section added!", fg="green", font=("calibri", 11)).pack()
+        if section_info == "":
+            section_success = Label(window, text="Can't be empty!", fg="orange", font=("calibri", 11)).place(x=150, y=280)
+        else:
+            file = open(userName, "a")
+            file.write(str(datetime.datetime.now()) + ", ")
+            file.write(section_info + "\n")
+            file.close()
+         
+            section_success = Label(window, text=" Section added!", fg="green", font=("calibri", 11)).place(x=150, y=280)
 
     window.protocol("WM_DELETE_WINDOW", on_closing)
 
-    message = tk.Label(window, text="Fine Leaf Count System", fg="black", width=85  ,
-                       height=2, font=('times', 30))
-    message.place(x=80, y=20)
+    message = tk.Label(window, text="Fine Leaf Count System", fg="black", bg="#539051", width=85, height=2, font=('times', 30, 'bold'))
+    message.place(x=80, y=10)
+
+    img = ImageTk.PhotoImage(Image.open("logo.png"))
+    panel = Label(window, image = img, bg='#539051')
+    panel.place(x=1350, y=10)
+    
+
+    startRecord = tk.Button(window, text="Start", command=video_stream, fg="white", bg="#539051", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
+    endRecord = tk.Button(window, text="Save & exit", command=end_video, fg="white", bg="#539051", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
+
 
     if is_login == True:
 
-        section = tk.Button(window, text="Add Section", command=section_gui, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
-        section.place(x=90, y=150) 
+        section_gui()
+        # section = tk.Button(window, text="Add Section", command=section_gui, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
+        # section.place(x=90, y=150) 
 
-        register = tk.Button(window, text="Register", command=register, fg="black", bg="salmon", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
-        register.place_forget()
-
-        signin = tk.Button(window, text="Login", command=login, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
+        signin = tk.Button(window, text="Login", command=login, fg="black", bg="#539051", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
         signin.place_forget()
 
         txt = "Welcome, " + userName.title()
         Label(window, text=txt, font=('times', 15, 'bold'), bg='white').place(x=1550, y=130)
 
-        startRecord = tk.Button(window, text="Start", command=video_stream, fg="black", bg="Silver", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
         startRecord.place(x=90, y=450)
 
-        endRecord = tk.Button(window, text="Finished", command=end_video, fg="black", bg="Silver", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
         endRecord.place(x=90, y=550)
-        endRecord.configure(state="disabled")
+        endRecord.configure(state="disabled", bg="silver")
 
     else:
+        global panel_bg
+        img_bg = ImageTk.PhotoImage(Image.open("bg.png"))
+        panel_bg = Label(window, image = img_bg, bg='white')
+        panel_bg.place(x=80, y=250)
 
-        section = tk.Button(window, text="Add Section", command=section_gui, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
-        section.place_forget()
+        # section = tk.Button(window, text="Add Section", command=section_gui, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
+        # section.place_forget()
 
-        register = tk.Button(window, text="Register", command=register, fg="black", bg="salmon", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
-        register.place(x=700, y=300)
+        signin = tk.Button(window, text="Login", command=login, fg="black", bg="#539051", width=25,height=2, activebackground = "Grey" , font=('times', 20, 'bold'))
+        signin.place(x=1300, y=500)
 
-        signin = tk.Button(window, text="Login", command=login, fg="black", bg="deep sky blue", width=20,height=2, activebackground = "Grey" , font=('times', 15, 'bold'))
-        signin.place(x=1000, y=300)
-
-        startRecord = tk.Button(window, text="Start", command=video_stream, fg="black", bg="Silver", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
         startRecord.place_forget()
 
-        endRecord = tk.Button(window, text="Finished", command=end_video, fg="black", bg="Silver", width=20,height=3, activebackground = "Grey" , font=('times', 15, 'bold'))
         endRecord.place_forget()
-        endRecord.configure(state="disabled")
 
     window.mainloop()
 
@@ -300,6 +344,3 @@ if __name__ == '__main__':
         vp_start_gui()
 
     vp_start_gui()
-
-
-# datetime, username, userID, sectionID, FLC %age
