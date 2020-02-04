d8771b0d79d53d033bf1acedb34774f3d4e8da61
shweta-agnext <shweta.kumari@agnext.in>
2020-02-04 15:17:30 +0530

    fixed - cam init

--- flc_utils/guvcview-config/cam_initialise.py
+++ flc_utils/guvcview-config/cam_initialise.py
@@ -2,6 +2,6 @@ import time
 import subprocess
 
 p = subprocess.Popen("exec /usr/local/ecam_tk1/bin/ecam_tk1_guvcview --profile=/home/agnext/Documents/flc/flc_utils/guvcview-config/default.gpfl", stdout=subprocess.PIPE, shell=True)
-time.sleep(1)
+time.sleep(1.3)
 p.kill()
 
