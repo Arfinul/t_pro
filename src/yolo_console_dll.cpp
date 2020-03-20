e9a8ee3c7ca7eb179eccf50b89c94b5665aacf72
Agnext <shweta.kumari@agnext.in>
2020-03-20 15:55:13 +0530

    fixed - unexpected video closing

--- src/yolo_console_dll.cpp
+++ src/yolo_console_dll.cpp
@@ -237,73 +237,73 @@ int double_tap_seconds_close = 5;
 void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names,
     int current_det_fps = -1, int current_cap_fps = -1, bool black_screen=false)
 {
-    // UNCOMMENTED FOR DEVELOPER MODE
-    int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{ 1,1,0 },{ 1,0,0 } };       // Agnext
-    if (black_screen == false)
-    {
-        for (auto &i : result_vec) {
-            cv::Scalar color = obj_id_to_color(i.obj_id);
-            // cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
-            // if (obj_names.size() > i.obj_id) {
-            if (obj_names.size() > i.obj_id && result_vec.size() > 0) {
-                std::string obj_name = obj_names[i.obj_id];
-                if (i.track_id > 0) obj_name += " - " + std::to_string(i.track_id);
-                cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
-                int max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
-                max_width = std::max(max_width, (int)i.w + 2);
-                //max_width = std::max(max_width, 283);
-                std::string coords_3d;
-                if (!std::isnan(i.z_3d)) {
-                    std::stringstream ss;
-                    // ss << std::fixed << std::setprecision(2) << "x:" << i.x_3d << "m y:" << i.y_3d << "m z:" << i.z_3d << "m ";  // Agnext, originall uncommented
-                    coords_3d = ss.str();
-                    cv::Size const text_size_3d = getTextSize(ss.str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, 1, 0);
-                    int const max_width_3d = (text_size_3d.width > i.w + 2) ? text_size_3d.width : (i.w + 2);
-                    if (max_width_3d > max_width) max_width = max_width_3d;
-                }
-                std::string folder_name = "";
-                if (obj_names[i.obj_id] == "1Banjhi"){
-                    folder_name = "1bj/";
-                }
-                else if (obj_names[i.obj_id] == "2Banjhi"){
-                    folder_name = "2bj/";
-                }
-                else if (obj_names[i.obj_id] == "1LB"){
-                    folder_name = "1lb/";
-                }
-                else if (obj_names[i.obj_id] == "2LB"){
-                    folder_name = "2lb/";
-                }
-                else if (obj_names[i.obj_id] == "3LB"){
-                    folder_name = "3lb/";
-                }
-                else if (obj_names[i.obj_id] == "Coarse"){
-                    folder_name = "coarse/";
-                }
-                else if (obj_names[i.obj_id] == "Cluster"){
-                    folder_name = "cluster/";
-                }
-                std::string img_name = "reports/" + folder_name + obj_name + ".png";
-                std::ifstream infile(img_name);
-                if (infile.good()){}
-                else{
-                    cv::Mat crop = mat_img(cv::Rect(i.x, i.y, i.w, i.h));
-                    cv::imwrite(img_name, crop);
-                }
+    // // UNCOMMENTED FOR DEVELOPER MODE
+    // int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{ 1,1,0 },{ 1,0,0 } };       // Agnext
+    // if (black_screen == false)
+    // {
+    //     for (auto &i : result_vec) {
+    //         cv::Scalar color = obj_id_to_color(i.obj_id);
+    //         // cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
+    //         // if (obj_names.size() > i.obj_id) {
+    //         if (obj_names.size() > i.obj_id && result_vec.size() > 0) {
+    //             std::string obj_name = obj_names[i.obj_id];
+    //             if (i.track_id > 0) obj_name += " - " + std::to_string(i.track_id);
+    //             cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
+    //             int max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+    //             max_width = std::max(max_width, (int)i.w + 2);
+    //             //max_width = std::max(max_width, 283);
+    //             std::string coords_3d;
+    //             if (!std::isnan(i.z_3d)) {
+    //                 std::stringstream ss;
+    //                 // ss << std::fixed << std::setprecision(2) << "x:" << i.x_3d << "m y:" << i.y_3d << "m z:" << i.z_3d << "m ";  // Agnext, originall uncommented
+    //                 coords_3d = ss.str();
+    //                 cv::Size const text_size_3d = getTextSize(ss.str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, 1, 0);
+    //                 int const max_width_3d = (text_size_3d.width > i.w + 2) ? text_size_3d.width : (i.w + 2);
+    //                 if (max_width_3d > max_width) max_width = max_width_3d;
+    //             }
+    //             std::string folder_name = "";
+    //             if (obj_names[i.obj_id] == "1Banjhi"){
+    //                 folder_name = "1bj/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "2Banjhi"){
+    //                 folder_name = "2bj/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "1LB"){
+    //                 folder_name = "1lb/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "2LB"){
+    //                 folder_name = "2lb/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "3LB"){
+    //                 folder_name = "3lb/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "Coarse"){
+    //                 folder_name = "coarse/";
+    //             }
+    //             else if (obj_names[i.obj_id] == "Cluster"){
+    //                 folder_name = "cluster/";
+    //             }
+    //             std::string img_name = "reports/" + folder_name + obj_name + ".png";
+    //             std::ifstream infile(img_name);
+    //             if (infile.good()){}
+    //             else{
+    //                 cv::Mat crop = mat_img(cv::Rect(i.x, i.y, i.w, i.h));
+    //                 cv::imwrite(img_name, crop);
+    //             }
 
-                // cv::rectangle(mat_img, cv::Point2f(std::max((int)i.x - 1, 0), std::max((int)i.y - 35, 0)),
-                //     cv::Point2f(std::min((int)i.x + max_width, mat_img.cols - 1), std::min((int)i.y, mat_img.rows - 1)),
-                //     color, CV_FILLED, 8, 0);
-                // putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 0, 0), 2); // original
+    //             // cv::rectangle(mat_img, cv::Point2f(std::max((int)i.x - 1, 0), std::max((int)i.y - 35, 0)),
+    //             //     cv::Point2f(std::min((int)i.x + max_width, mat_img.cols - 1), std::min((int)i.y, mat_img.rows - 1)),
+    //             //     color, CV_FILLED, 8, 0);
+    //             // putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 0, 0), 2); // original
 
-                // std::string frame_str = "FRAME: " + std::to_string(frame_id);
-                // putText(mat_img, frame_str, cv::Point2f(10, 50), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 255, 255), 1);  // Agnext
+    //             // std::string frame_str = "FRAME: " + std::to_string(frame_id);
+    //             // putText(mat_img, frame_str, cv::Point2f(10, 50), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 255, 255), 1);  // Agnext
 
-                // putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 0, 0), 2); // Agnext
-                if(!coords_3d.empty()) putText(mat_img, coords_3d, cv::Point2f(i.x, i.y-1), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(0, 0, 0), 1);
-            }
-        }
-    }
+    //             // putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 0, 0), 2); // Agnext
+    //             if(!coords_3d.empty()) putText(mat_img, coords_3d, cv::Point2f(i.x, i.y-1), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(0, 0, 0), 1);
+    //         }
+    //     }
+    // }
     if (current_det_fps >= 0 && current_cap_fps >= 0) {
 
         std::string fps_str = "FPS detection: " + std::to_string(current_det_fps) + "   FPS capture: " + std::to_string(current_cap_fps);
