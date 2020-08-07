fea04aad68f7db06a6e9b53f04b00223f0e7dacb
Agnext <shweta.kumari@agnext.in>
2020-08-07 15:00:38 +0530

    fix - bug

--- app.py
+++ app.py
@@ -658,7 +658,7 @@ class MyTkApp(tk.Frame):
         text_result = ''
         for i in result:
             text_result += i + ': ' + result[i] + '\n'
-        self._flc_btn.configure(text="Result    " + json.dumps(result))
+        self._flc_btn.configure(text="Result    " + result)
         self._flc_btn.place(x=60,y=130)
         gc.collect()
              
