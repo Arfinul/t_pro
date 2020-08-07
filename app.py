7cfc3aecbf43f4862ec61f88b6163b4547582a20
Agnext <shweta.kumari@agnext.in>
2020-08-07 15:06:13 +0530

    fix - bug

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ class MyTkApp(tk.Frame):
         self.forget_graph()
         text_result = ''
         for i in result:
-            text_result += i + ': ' + result[i] + '\n'
+            text_result += i + ': ' + str(result[i]) + '\n'
         self._flc_btn.configure(text="Result    " + result)
         self._flc_btn.place(x=60,y=130)
         gc.collect()
