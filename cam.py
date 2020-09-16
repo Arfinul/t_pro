0d7e78ee7f6db5a534a5f65ac7088d0d0ad06858
Agnext <shweta.kumari@agnext.in>
2020-09-16 12:57:39 +0530

    updated - path do documents

--- cam.py
+++ cam.py
@@ -1,7 +1,7 @@
 import time
 import subprocess
 
-p = subprocess.Popen("exec /usr/local/ecam_tk1/bin/ecam_tk1_guvcview --profile=/home/agnext/Downloads/flc/default.gpfl", stdout=subprocess.PIPE, shell=True)
+p = subprocess.Popen("exec /usr/local/ecam_tk1/bin/ecam_tk1_guvcview --profile=/home/agnext/Documents/flc/default.gpfl", stdout=subprocess.PIPE, shell=True)
 time.sleep(1.5)
 p.kill()
 
